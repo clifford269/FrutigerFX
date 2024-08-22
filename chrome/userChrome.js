a3109ff82afa021ff4a1d1e217d538ba4909fccf
Cliffy <83859194+clifford269@users.noreply.github.com>
2024-08-22 18:14:39 +0530

    Improvisation

--- chrome/userChrome.js
+++ chrome/userChrome.js
@@ -1,3 +1,4 @@
 userChrome.import("/userChrome/favicon_in_urlbar.uc.js", "UChrm");
 userChrome.import("/userChrome/tab_label_in_urlbar.uc.js", "UChrm");
 userChrome.import("/userChrome/custom_scrollbars.uc.js", "UChrm")
+userChrome.import("/userChrome/default_favicon.js", "UChrm")
