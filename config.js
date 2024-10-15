1d0454710bb330abb5f26c0959dbb1e23c7e7e77
Greatness <83859194+clifford269@users.noreply.github.com>
2024-10-16 03:16:57 +0530

    .

--- config.js
+++ config.js
@@ -1,17 +1,33 @@
-// config.js 
+// skip 1st line
 
-try {
+  let {
+    classes: Cc,
+    interfaces: Ci,
+    manager: Cm,
+    utils: Cu
+  } = Components;
   Cu.importGlobalProperties(['PathUtils']);
 
-  if (!Services.appinfo.inSafeMode) {
-    Services.scriptloader.loadSubScript(
+
+  Services.scriptloader.loadSubScript(
       PathUtils.toFileURI(PathUtils.join(PathUtils.profileDir,
-      'chrome', 'userChrome', 'userChromeJS.js')), this, 'UTF-8'
-    );
+          'chrome', 'userChrome', 'userChromeJS.js')), this, 'UTF-8'
+  );
+
+
+  let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+  cmanifest.append('utils');
+  cmanifest.append('chrome.manifest');
+  
+  if(cmanifest.exists()){
+    Cm.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
+    Cu.import('chrome://userchromejs/content/boot.jsm');
   }
-} catch(e) {
-	Components.utils.reportError(e);
-};
+
+
+
+
+
 defaultPref("ui.systemUsesDarkMode", 0);
 defaultPref("browser.theme.dark-private-windows", false);
 defaultPref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
